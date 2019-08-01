2cffbc562e6f6e63fd6d787454598086793a562a
sharpscar <sharpscar@gmail.com>
2019-08-01 16:35:31 +0900

    ??

---
+++ job-dsl/nodejsdocker.groovy
@@ -0,0 +1,26 @@
+job('NodeJS Docker example'){
+    scm {
+        git('git://github.com/sharpscar/simple_node_helloworld'){
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('sharpscar@gmail.com')
+        }
+    }
+
+    triggers{
+        scm('H/5 * * * *')
+    }
+    wrappers{
+        nodejs('nodejs')
+    }
+    steps{
+        dockerBuildAndPublish{
+            repositoryName('sharpscar/docker-nodejs-demo')
+            tag('${GIT_REVISION, length-9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
